4aa1b478e6de5e88e0852e5dbca67fbe24c4f355
Vincent <p0918981461@gmail.com>
2022-06-30 16:48:03 +0800

    導入EntityFrameworkCore套件、建立員工Model、DBContext

---
+++ EmployeeManagement/EmployeeManagement/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        /// <summary>
+        /// 員工代碼
+        /// </summary>
+        [Key]
+        public int EmployeeId { get; set; }
+
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        [Column(TypeName = "NVARCHAR(100)")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 部門編號
+        /// </summary>
+        [Column(TypeName = "NVARCHAR(100)")]
+        public string Dept { get; set; }
+    }
+}
